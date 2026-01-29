2fed04e6ba2e1dc82aa7e0226bd296292463cd1d
shivakumar109 <ramidishivakumarreddy@gmail.com>
2026-01-29 21:10:57 +0530

    Delete Week 2 directory

--- Week 2/agecal.js
+++
@@ -1,8 +0,0 @@
-// Assignment 3: Age Calculator (Intermediate)
-// -------------------------------------------
-// Input:
-let dob = new Date("2000-05-15");
-// Tasks:
-//         1. Calculate exact age in years
-let now=new Date()
-console.log("Age=",now.getFullYear()-dob.getFullYear())
